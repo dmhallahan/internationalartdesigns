954ccf52d42f4aa523339c601e0a014a62562122
David Hallahan <dmhallahan@hotmail.com>
2011-06-26 19:24:57 -0400

    small refinements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,17 @@ def is_current_page?(page_name)
 end
 
 def is_current_controller?(controller_name)
-  params[:path].include? controller_name unless params[:path].nil? 
+  unless params[:path].nil? 
+     "highlight" if params[:path].include? controller_name
+  end
+end
+
+def is_referer?(referer_page)
+   "highlight" if request.referer.include? referer_page
+end
+
+def is_collection_page?
+ "highlight" if is_current_controller?("collection") or is_current_page?("detail")
 end
 
 end
